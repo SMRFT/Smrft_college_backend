198fca016438bab7fcc6f0ce75e04c32a89348c4
SMRFT <110091550+SMRFT@users.noreply.github.com>
2026-03-04 16:58:27 +0530

    Update urls.py

--- smrft_backend/urls.py
+++ smrft_backend/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('smrft.url')),
+    path('_b_a_c_k_e_n_d/Smrft/', include('smrft.url')),
 ]
+
