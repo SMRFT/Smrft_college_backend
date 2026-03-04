b153fc4c77bad1e02fc72f50b2628a0e8fbc3690
SMRFT <110091550+SMRFT@users.noreply.github.com>
2026-03-04 17:01:04 +0530

    Update CORS_ALLOWED_ORIGINS to include Netlify app
    Added a new allowed origin for CORS in the settings.

--- smrft_backend/settings-test.py
+++ smrft_backend/settings-test.py
@@ -111,10 +111,12 @@ STATIC_URL = '/static/'
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',  # React development server
+    "https://shanmuganursingcollege.netlify.app",
 ]
 
 # File Upload Settings
 DATA_UPLOAD_MAX_MEMORY_SIZE = 20 * 1024 * 1024  # 20MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 20 * 1024 * 1024  # 20MB
+
 
 
